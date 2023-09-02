277301a9130325f4c22be4af4a073c201b4da38d
YongKang Sun <kangocoeuc@hotmail.com>
2023-09-02 09:42:23 +0800

    final version

--- import.py
+++ import.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+start = input('请决定随机数字范围初始值: ')
+end = input('请决定随机数字的结束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1
